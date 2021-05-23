e04dfe1c974cbf6b83905537f1c31e3ec467a696
HubertRadom <hub.rad1@wp.pl>
2021-05-23 15:54:29 +0200

    update

---
+++ server.r
@@ -0,0 +1,241 @@
+library(shiny)
+library(shinythemes)
+library(DT)
+library(ggplot2)
+library(plyr)
+library(dplyr)
+
+library(ggridges)
+library(viridis)
+library(hrbrthemes)
+library(RColorBrewer)
+
+require(gridExtra)
+
+d_math=read.table("www/student-mat.csv",sep=",",header=TRUE)
+d_por=read.table("www/student-por.csv",sep=",",header=TRUE)
+
+d_math$sum_alcohol <- d_math$Dalc + d_math$Walc
+d_por$sum_alcohol <- d_por$Dalc + d_por$Walc
+
+dfs=list(d_math,d_por)
+
+shinyServer(function(input, output) {
+  
+  output$RawData <- DT::renderDataTable(
+    
+    DT::datatable({
+      select( dfs[[strtoi(input$expl_subject)]] ,school,sex,age,address,famsize,Pstatus,Medu,Fedu,Mjob,Fjob)
+    },
+    options = list(lengthMenu=list(c(5,10,15),c('5','10','15')),pageLength=5,
+                   initComplete = JS(
+                     "function(settings, json) {",
+                     "$(this.api().table().header()).css({'background-color': 'moccasin', 'color': '1c1b1b'});",
+                     "}"),
+                   columnDefs=list(list(className='dt-center',targets="_all"))
+    ),
+    filter = "top",
+    selection = 'multiple',
+    style = 'bootstrap',
+    class = 'cell-border stripe',
+    rownames = FALSE,
+    colnames = c("School","Sex","Age","Address","Family size", "Parent status","Mother's edu","Father's edu","Mother's job","Father's job")
+    ))
+
+  
+  output$explorePlot = renderPlot({
+    
+    
+    sub = strtoi(input$expl_subject)
+    
+    set.seed(123)
+    ggplot(dfs[[sub]], aes_string(x = input$expl_grade, y = "sum_alcohol", color=input$expl_factor)) +
+      geom_jitter() +
+      geom_point(data = dfs[[sub]][input$RawData_rows_selected,], aes_string(x = input$expl_grade, y = "sum_alcohol"), size=5, color="black") + 
+      theme_classic() +
+      stat_smooth(method = "lm",
+                  col = "#C42126",
+                  se = FALSE,
+                  size = 1.5) +
+      geom_smooth(method="lm")
+    
+  })
+  
+  
+  output$relationshipPlot <- renderPlot({
+    
+    sub = strtoi(input$rel_subject)
+    temp_data = dfs[[sub]]
+    temp_data$Dalc <- as.factor(temp_data$Dalc)
+    temp_data$Walc <- as.factor(temp_data$Walc)
+    
+    ggplot(data = filter(temp_data, romantic == input$rel_romantic), aes_string(x = "famrel", fill = input$rel_alcohol)) +
+      geom_bar(position = "fill") + ylab("proportion") + xlab("family relations") + labs(fill = "alcohol consumption") +
+      stat_count(geom = "text", 
+                 aes(label = stat(count)),
+                 position=position_fill(vjust=0.5), colour="white")
+    
+  })
+  
+  
+  output$gradesHistPlot <- renderPlot({ 
+    
+    sub = strtoi(input$grades_subject)
+    temp_data = dfs[[sub]]
+    
+    if (input$grades_alcohol == "Walc") {
+      temp_data$split = ifelse(temp_data$Walc >= strtoi(input$grades_split), "more", "less")
+    } else {
+      temp_data$split = ifelse(temp_data$Dalc >= strtoi(input$grades_split), "more", "less")
+    }
+    
+    if (input$grades_grade == "G1") {
+      cdat <- ddply(temp_data, "split", summarise, mean=mean(G1))
+    } else if (input$grades_grade == "G2") {
+      cdat <- ddply(temp_data, "split", summarise, mean=mean(G2))
+    } else if (input$grades_grade == "G3") {
+      cdat <- ddply(temp_data, "split", summarise, mean=mean(G3))
+    }
+    
+    ggplot(temp_data, aes_string(x=input$grades_grade, fill="split")) +
+      geom_histogram(binwidth=1, alpha=.5, position="identity") + 
+      geom_vline(data=cdat, aes_string(xintercept="mean",  colour="split"),
+                 linetype="dashed", size=1)
+    
+  })
+  
+  output$gradesBoxPlot <- renderPlot({
+    
+    sub = strtoi(input$grades_subject)
+    temp_data = dfs[[sub]]
+    
+    if (input$grades_alcohol == "Walc") {
+      temp_data$split = ifelse(temp_data$Walc >= strtoi(input$grades_split), "more", "less")
+    } else {
+      temp_data$split = ifelse(temp_data$Dalc >= strtoi(input$grades_split), "more", "less")
+    }
+    
+    
+    ggplot(temp_data, aes_string(x="split", y=input$grades_grade, fill="split")) + geom_boxplot() +
+      guides(fill=FALSE)
+    
+  })
+  
+  
+  
+  output$gradesC <- renderPlot({
+    
+    sub = strtoi(input$subj)
+    temp_data = dfs[[sub]]
+    
+    temp_data$sex <- as.factor(temp_data$sex)
+    temp_data$Pstatus <- as.factor(temp_data$Pstatus)
+    
+    ggplot(temp_data[temp_data[, "age"]>=input$age_range[1] & temp_data[, "age"]<=input$age_range[2], ], aes_string(x="sex", y=input$grades_gradeT, fill="Pstatus")) + 
+      geom_violin(trim=T) +
+      ylim(0, 20)
+    
+
+    
+  })
+  
+  output$gradesSTR <- renderPlot({
+    
+    sub = strtoi(input$subjS)
+    temp_data = dfs[[sub]]
+    
+    temp_data$studytime <- as.factor(temp_data$studytime)
+    
+    
+    
+    p1 <- ggplot(data = filter(temp_data, romantic == input$rel_romanticS & sex == "M"), aes_string(x = input$grades_gradeS, y = "studytime", fill= "..x..")) + 
+      geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+      labs(title = "Boys' score") + 
+      scale_fill_viridis(name = "Score", option = "C") +
+      theme_ipsum() + 
+      theme(
+        panel.spacing = unit(0.1, "lines"),
+        strip.text.x = element_text(size=8)
+      )
+    
+    p2 <- ggplot(data = filter(temp_data, romantic == input$rel_romanticS & sex == "F"), aes_string(x = input$grades_gradeS, y = "studytime", fill="..x..")) + 
+      geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+      labs(title = "Girls' score") + 
+      scale_fill_viridis(name = "Score", option = "C") +
+      theme_ipsum() + 
+      theme(
+        panel.spacing = unit(0.1, "lines"),
+        strip.text.x = element_text(size=8)
+      )
+    
+    grid.arrange(p1, p2, ncol = 2)
+    
+    
+  })
+  
+  output$Pass <- renderPlot({
+    
+    sub = strtoi(input$subjP)
+    t_data = dfs[[sub]]
+    
+    
+    myPalette <- brewer.pal(5, "Set2")
+    
+    
+    column = c()
+    for (val in t_data[,"failures"] == 0){
+      if (val){
+        column <-  c(column, "Pass")
+      }
+      else{
+        column <-  c(column, "Fail")
+      }
+    }
+    t_data$lucky <- column
+    sum(t_data$lucky == "Fail")
+    
+    ###
+    grid <- matrix(c(1, 1, 2, 3), nrow = 2, ncol = 2, byrow = T)
+    layout(grid)
+    
+    x = input$sexP
+    dt = t_data
+    if(x == "F"){
+      dt = t_data[t_data[, "sex"] == "F",]
+    }
+    if(x == "M"){
+      dt = t_data[t_data[, "sex"] == "M",]
+    }
+    
+    data_yes = list(c("yes", "yes"), c("yes", "no"), c("no", "yes"))
+    
+    da = data_yes[[strtoi(input$activities)]]
+    
+    data_temp = dt[dt[, "activities"] == da[1] | dt[, "activities"] != da[2],]
+    
+    
+    prop <- c(sum(data_temp[,"lucky"] == "Pass" ), sum(data_temp[,"lucky"] == "Fail"))
+    p1 <- pie(prop, labels = c("Pass", "Fail"), col = myPalette, main = "Extra-curicular activities")
+    
+    ###
+    
+    
+    dr = data_yes[[strtoi(input$rel_romanticP)]]
+    data_temp1 = data_temp[data_temp[, "romantic"] == dr[1] | data_temp[, "romantic"] != dr[2],]
+    
+    prop <- c(sum(data_temp1[,"lucky"] == "Pass" ), sum(data_temp1[,"lucky"] == "Fail"))
+    p2 <- pie(prop, labels = c("Pass", "Fail"), col = myPalette, main = "Romantic relationship")
+    
+    ##
+    di = data_yes[[strtoi(input$internetP)]]
+    data_temp2 = data_temp[data_temp[, "internet"] == di[1] | data_temp[, "internet"] != di[2],]
+    
+    prop <- c(sum(data_temp2[,"lucky"] == "Pass"), sum(data_temp2[,"lucky"] == "Fail"))
+    p3 <- pie(prop, labels = c("Pass", "Fail"), col = myPalette, main = "Internet access")
+    
+    
+  })
+  
+  
+})
+
---
+++ ui.r
@@ -0,0 +1,265 @@
+library(shiny)
+library(shinythemes)
+library(DT)
+library(ggplot2)
+
+shinyUI(fluidPage(theme = shinytheme("cerulean"),
+                  
+                  titlePanel("Student Alcohol Consumption"),
+                  navbarPage("Let's get started",
+                             tabPanel("Explore",
+                                      fluidRow(column(tags$img(src="pp_logo.png",width="240px",height="240px"),width=2),
+                                               column(
+                                                 br(),
+                                                 p("This dashboard is a project for the Data Visualization course at the Poznań University of Technology. 
+                                   It allows for an interactive analysis of the selected dataset. 
+                                   It was made using RStudio and shiny. Visualizations were made in ggplot2. 
+                                     ",style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
+                                                 br(),
+                                                 
+                                                 p("The data used in this application are publicly available on the Kaggle. The data were obtained in a survey of students math and portuguese language courses in secondary school. 
+                                     It contains a lot of interesting social, gender and study information about students.",
+                                                   style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
+                                                 width=8),
+                                               column(
+                                                 br(),
+                                                 p("To find more about",em("Student Alcohol Consumption"),"dataset click below.",
+                                                   br(),
+                                                   a(href="https://www.kaggle.com/uciml/student-alcohol-consumption", "Go to Kaggle",target="_blank"),style="text-align:center;color:black"),
+                                                 #  width=2),
+                                                 br(),
+                                                 
+                                                 
+                                                 p("To check out ",em("source code")," click below.",
+                                                   br(),
+                                                   a(href="https://www.kaggle.com/uciml/student-alcohol-consumption", "Go to Github",target="_blank"),style="text-align:center;color:black"),
+                                                 width=2),
+                                               
+                                               br()
+                                      ),
+                                      
+                                      fluidRow(  
+                                        sidebarLayout(
+                                          sidebarPanel(
+                                            
+                                            # Input: Slider for the number of bins ----
+                                            radioButtons(inputId = "expl_subject",
+                                                         label = "Subject:",
+                                                         choices = list("Math"=1,"Portuguese"=2)),
+                                            radioButtons(inputId = "expl_grade:",
+                                                         label = "Grade period:",
+                                                         choices = list("G1"="G1","G2"="G2","G3"="G3")),
+                                            selectInput(inputId = "expl_factor",
+                                                        label = "Select factor:",
+                                                        choices = list("School"="school","Sex"="sex","Address"="address","Family size"="famsize"
+                                                                       ,"Parent status"="Pstatus", "Reason to choose school"="reason",
+                                                                       "Guardian"="guardian","Extra educational support"="schoolsup",
+                                                                       "Family educational support"="famsup","Extra paid classes"="paid",
+                                                                       "Internet"="internet","Romantic relationship"="romantic")),
+                                          ),
+                                          
+                                          
+                                          mainPanel(
+                                            plotOutput('explorePlot')
+                                          )
+                                        ),
+                                        
+                                        fluidRow(column(DT::dataTableOutput("RawData"),
+                                                        width = 12),
+                                        ), fluidRow(
+                                          p("Attributes for select factor: ",br(),"
+
+                                            school - student's school (binary: 'GP' - Gabriel Pereira or 'MS' - Mousinho da Silveira)",br(),"
+                                            sex - student's sex (binary: 'F' - female or 'M' - male)",br(),"
+                                            address - student's home address type (binary: 'U' - urban or 'R' - rural)",br(),"
+                                            famsize - family size (binary: 'LE3' - less or equal to 3 or 'GT3' - greater than 3)",br(),"
+                                            Pstatus - parent's cohabitation status (binary: 'T' - living together or 'A' - apart)",br(),"
+                                            reason - reason to choose this school (nominal: close to 'home', school 'reputation', 'course' preference or 'other')",br(),"
+                                            guardian - student's guardian (nominal: 'mother', 'father' or 'other')",br(),"
+                                            schoolsup - extra educational support (binary: yes or no)",br(),"
+                                            famsup - family educational support (binary: yes or no)",br(),"
+                                            paid - extra paid classes within the course subject (Math or Portuguese) (binary: yes or no)",br(),"
+                                            internet - Internet access at home (binary: yes or no)",br(),"
+                                            romantic - with a romantic relationship (binary: yes or no)",br(),br(),"
+
+                                            These grades are related with the course subject, Math or Portuguese:",br(),"
+                                            
+                                            G1 - first period grade (numeric: from 0 to 20)",br(),"
+                                            G2 - second period grade (numeric: from 0 to 20)",br(),"
+                                            G3 - final grade (numeric: from 0 to 20, output target)",br(),"
+                                        ",
+                                            style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
+                                          width=8)
+                                      )
+                             ),
+                             
+                             tabPanel("Relationships",
+                                      sidebarLayout(
+                                        sidebarPanel(
+                                          
+                                          radioButtons(inputId = "rel_subject",
+                                                       label = "Subject:",
+                                                       choices = list("Math"=1,"Portuguese"=2)),
+                                          radioButtons(inputId = "rel_alcohol",
+                                                       label = "Alcohol consumption time:",
+                                                       choices = list("Workday"="Dalc","Weekend"="Walc")),
+                                          radioButtons(inputId = "rel_romantic",
+                                                       label = "In a romantic relationship:",
+                                                       choices = list("Yes"="yes","No"="no"))
+                                        ),
+                                        
+                                        
+                                        mainPanel(
+                                          
+                                          plotOutput(outputId = "relationshipPlot")
+                                          
+                                        )
+                                      ), fluidRow(
+                                        p("Analyzing the data, we can see that people who have poor relationships with their family have a higher proportion of alcohol consumption above 1 than those who have better relationships. This may indicate that people with problems at home are more likely to drink alcohol. However, such dependence is not so apparent depending on whether or not they are in a romantic relationship. ",
+                                          style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
+                                        width=8)
+                                      
+                             ),
+                             
+                             tabPanel("Grades distribution",
+                                      sidebarLayout(
+                                        
+                                        sidebarPanel(
+                                          
+                                          radioButtons(inputId = "grades_subject",
+                                                       label = "Subject:",
+                                                       choices = list("Math"=1,"Portuguese"=2)),
+                                          radioButtons(inputId = "grades_alcohol",
+                                                       label = "Alcohol consumption time:",
+                                                       choices = list("Workday"="Dalc","Weekend"="Walc")),
+                                          radioButtons(inputId = "grades_grade:",
+                                                       label = "Grade period:",
+                                                       choices = list("G1"="G1","G2"="G2","G3"="G3")),
+                                          sliderInput("grades_split", label = h3("Split:"), min = 1, 
+                                                      max = 5, value = 3)
+                                        ),
+                                        
+                                        
+                                        mainPanel(
+                                          
+                                          plotOutput(outputId = "gradesHistPlot"),
+                                          
+                                          plotOutput(outputId = "gradesBoxPlot")
+                                          
+                                        )
+                                      ), fluidRow(
+                                        p("The split divides students into those who have alcohol consumption of a certain level or more. The relationship resulting from this analysis is obvious - students who drink alcohol more often have lower grades. ",
+                                          style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
+                                        
+                                        width=8)
+                             ),
+                             tabPanel("Grades and cohabitation",
+                                      sidebarLayout(
+                                        
+                                        sidebarPanel(
+                                          
+                                          radioButtons(inputId = "subj",
+                                                       label = "Subject:",
+                                                       choices = list("Math"=1,"Portuguese"=2)),
+                                          
+                                          radioButtons(inputId = "grades_gradeT:",
+                                                       label = "Grade period:",
+                                                       choices = list("G1"="G1","G2"="G2","G3"="G3")),
+                                          
+                                          sliderInput("age_range", label = h3("Age range:"), min = 15, 
+                                                      max = 19, value = c(15,19))
+                                        ),
+                                        
+                                        
+                                        mainPanel(
+                                          
+                                          plotOutput(outputId = "gradesC")
+                                          
+                                        )
+                                      ), fluidRow(
+                                        p("Boys that were living with a single parent(their parents lived apart) had
+                                          better scores.
+                                          The same, but the very opposite happend to girls' scores, when the parents
+                                          lived together their scores were better.",
+                                          style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
+                                        width=8)
+                             ),
+                             tabPanel("Study Time",
+                                      sidebarLayout(
+                                        
+                                        sidebarPanel(
+                                          
+                                          radioButtons(inputId = "subjS",
+                                                       label = "Subject:",
+                                                       choices = list("Math"=1,"Portuguese"=2)),
+                                          
+                                          radioButtons(inputId = "grades_gradeS:",
+                                                       label = "Grade period:",
+                                                       choices = list("G1"="G1","G2"="G2","G3"="G3")),
+                                          
+                                          radioButtons(inputId = "rel_romanticS",
+                                                       label = "In a romantic relationship:",
+                                                       choices = list("Yes"="yes","No"="no"))
+                                        ),
+                                        
+                                        mainPanel(
+                                          
+                                          plotOutput(outputId = "gradesSTR")
+                                        )
+                                      ), fluidRow(
+                                        p(" Boys that were in the romantic relationship had slightly better scores
+                                        even though they spent less time studying(the disappear of the 4th(highest)
+                                        category of time spent studying indicates that).
+                                        And their score's became 'more stable'.",
+                                          style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
+                                        width=8)
+                             ),
+                             tabPanel("Pie",
+                                      sidebarLayout(
+                                        
+                                        sidebarPanel(
+                                          
+                                          radioButtons(inputId = "subjP",
+                                                       label = "Subject:",
+                                                       choices = list("Math"=1,"Portuguese"=2)),
+                                          
+                                          radioButtons(inputId = "sexP",
+                                                       label = "Sex",
+                                                       choices = list("Doesn't matter"="W", "Female"="F","Male"="M")),
+                                          
+                                          radioButtons(inputId = "activities",
+                                                       label = "extra-curricular",
+                                                       choices = list("Doesn't matter" = 1, "Yes"=2,"No"=3)),
+                                          
+                                          radioButtons(inputId = "rel_romanticP",
+                                                       label = "In a romantic relationship:",
+                                                       choices = list("Doesn't matter" = 1, "Yes"=2,"No"=3)),
+                                          
+                                          
+                                          radioButtons(inputId = "internetP",
+                                                       label = "Internet access",
+                                                       choices = list("Doesn't matter"= 1, "Yes"= 2,"No"= 3))
+                                          
+                                          
+                                        ),
+                                        
+                                        mainPanel(
+                                          
+                                          plotOutput(outputId = "Pass")
+                                          
+                                        )
+                                      ), fluidRow(
+                                        p("    Boys that weren't taking any extra lessons in romantic relationship
+                                        has the biggest chance of failing at least once in math course.
+                                        
+                                        Girls in a romantic relationships have much greater chance of failing at
+                                        least once in the math course, it didn't matter whether they had any extra lessons.
+                                        
+                                        Boys in a romantic relationship that were taking extra lessons had higher chance
+                                        of passing all the math exams.",
+                                          style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
+                                        width=8)
+                             )
+                  )
+)
+)
